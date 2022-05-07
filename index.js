3c0876ebcbcf3485a9c2207ce980f7643d889218
baard <bard.ove.hoel@kartverket.no>
2022-05-08 00:18:16 +0200

    testing async wait

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const https = require('https')
 
-try {
-    // Fetching github token
-    const jwt = core.getIDToken();
-    core.setOutput("jwt", jwt);
-    
-    
-    // Get the JSON webhook payload for workflow.
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    
-    console.log(`The event jwt: ${jwt}`);
-    
+async function jwt() {
+    try {
+        // Fetching github token
+        const jwt = await core.getIDToken();
+        core.setOutput("jwt", jwt);
+      
+      
+        // Get the JSON webhook payload for workflow.
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
     } catch (error) {
         core.setFailed(error.message);
-}    
+      }    
+}
     
+jwt();
+
 
 
 const vaultaddr = core.getInput('vaultaddr')
@@ -25,6 +27,8 @@ const options = {
     port: 443,
     method: 'GET'
 };
+
+await jwt();
 
 const req = https.request(options, res => {
     console.log(`statusCode: ${res.statusCode}`);
