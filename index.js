543e1fb4b708c9f53f44b2fe9cd5c1df399ed336
baard <bard.ove.hoel@kartverket.no>
2022-05-09 12:44:39 +0200

    added https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
-
+const https = require('https')
 
 var cb64 = core.getInput('certb64');
    
