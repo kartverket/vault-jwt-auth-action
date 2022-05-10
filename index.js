ae14b9a9cc82b9d602981816e54519e54f150eae
Bård Ove Hoel <bard.ove.hoel@kartverket.no>
2022-05-10 09:23:17 +0200

    test

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ async function jwt() {
       core.setOutput("jwt", jwt);
       
       // Get the JSON webhook payload for the event that triggered the workflow
-      //const payload = JSON.stringify(github.context.payload, undefined, 2)
-      //console.log(`The event payload: ${payload}`);
+      const payload = JSON.stringify(github.context.payload, undefined, 2)
+      console.log(`The event payload: ${payload}`);
     } catch (error) {
       core.setFailed(error.message);
     }
