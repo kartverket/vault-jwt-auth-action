5b576608b2f092edda92794194d079bd5dfafcb6
Bård Ove Hoel <bard.ove.hoel@kartverket.no>
2022-05-10 09:59:55 +0200

    test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function run() {
       // Get the JSON webhook payload for the event that triggered the workflow
       //const payload = JSON.stringify(github.context.payload, undefined, 2)
       //console.log(`The event payload: ${payload}`);
-
+      console.log('this is the jwt: ' , jwt)
       return jwt
     } catch (error) {
       console.log('this is jwt function')
